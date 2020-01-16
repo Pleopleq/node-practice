9b4516d76ae72d609ee9efa515bfec0d85042df6
Felix Anducho <felix.anducho74@gmail.com>
2020-01-15 18:20:19 -0600

    add comments

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ fs.readdir(process.cwd(), async (err, filenames) =>{
     if (err){
         console.log(err);
     }
-
+    //filling the lstat object with the filenames of the directory
     const statPromises = filenames.map(filename => {
         return lstat (filename);
     })
-
+//resolving all the promises with promise.all
 const allStats = await Promise.all(statPromises);
 
+//printing the filenames inside of allStats and putting green color to
+//files and blue to directories
 for(let stats of allStats){
     const index = allStats.indexOf(stats);
     if (stats.isFile()){
